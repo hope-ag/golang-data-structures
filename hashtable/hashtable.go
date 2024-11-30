ba095b6cbfc50c02f7f0764f06d01d1cc0431452
Hope <62467666+hope-ag@users.noreply.github.com>
2024-11-30 20:02:24 +0000

    init: hash tables

---
+++ hashtable/hashtable.go
@@ -0,0 +1,6 @@
+package hashtable
+
+// Collision handling: 
+// Open Addressing - item is placed in next available slot. Speed advantage is reduced as more collisions are avoided.
+// Separate Chaining - each bucket is a linked list.
+
---
+++ hashtable/separate_chaining.go
@@ -0,0 +1,3 @@
+package hashtable
+
+const ArraySize = 7
